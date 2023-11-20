f06496ea086406944f96e8a0716ddb8387245454
Wanderson Rodrigues <147121602+wandersonhrodrigues@users.noreply.github.com>
2023-11-20 12:29:01 -0300

    Criação da replica do site da Apple

---
+++ site_iPhone/js/scripts.js
@@ -0,0 +1,13 @@
+
+// const btnColor = document.querySelector('.trocar-cor')
+const imgIphone = document.querySelector('.iphone')
+const circulo = document.querySelector('.circulo')
+
+
+function trocarCor(cor){
+    circulo.style.background = cor
+}
+
+function trocarimg(img){
+    imgIphone.src = img
+}
